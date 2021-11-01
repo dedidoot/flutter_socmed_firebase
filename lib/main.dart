892da8af6dbfebc312700343ec328a094ff5b301
dedi <dedidot26@gmail.com>
2021-11-02 00:09:31 +0700

    social media

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/welcome_screen.dart';
-import 'package:flash_chat/screens/login_screen.dart';
-import 'package:flash_chat/screens/registration_screen.dart';
-import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/pages/home.dart';
 
-void main() => runApp(FlashChat());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  /*Firestore.instance.settings(timestampsInSnapshotsEnabled: true).then((_) {
+    // print("Timestamps enabled in snapshots\n");
+  }, onError: (_) {
+    // print("Error enabling timestamps in snapshots\n");
+  });*/
+  runApp(MyApp());
+}
 
-class FlashChat extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
+      title: 'Social Media',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        accentColor: Colors.lightGreen,
       ),
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        WelcomeScreen.id : (context) => WelcomeScreen(),
-        LoginScreen.id : (context) => LoginScreen(),
-        RegistrationScreen.id : (context) => RegistrationScreen(),
-        ChatScreen.id : (context) => ChatScreen(),
-      },
+      home: Home(),
     );
   }
 }
